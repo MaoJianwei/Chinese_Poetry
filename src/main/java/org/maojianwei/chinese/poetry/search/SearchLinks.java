267d5277316583cc1fddef58a30a6de827db21b9
maojianwei <maojianwei2016@126.com>
2016-04-08 22:28:23 +0800

    get first page links OK!

---
+++ src/main/java/org/maojianwei/chinese/poetry/search/SearchLinks.java
@@ -0,0 +1,63 @@
+package org.maojianwei.chinese.poetry.search;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.swing.text.html.HTML;
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Hello world!
+ */
+public class SearchLinks {
+    public static final String poetryUrlHead = "http://so.gushiwen.org";
+
+    public static void main(String[] args) {
+        searchLinks();
+    }
+
+    public static void searchLinks() {
+
+        LinkedBlockingQueue linkQueue = new LinkedBlockingQueue();
+
+        String pageLink = "http://so.gushiwen.org/type.aspx";
+
+        while (!pageLink.equals("")) {
+            try {
+                Document doc = Jsoup.connect("http://so.gushiwen.org/type.aspx").get();
+
+                Elements elements = doc.getElementsByClass("sons");
+
+                for (Element div : elements) {
+
+                    for (Element ele : div.children()) {
+                        if (!ele.tag().getName().equals("p")) {
+                            continue;
+                        } else {
+                            if (ele.children() != null) {
+                                String poetryLink = ele.child(0).attr("href");
+                                if (!poetryLink.equals("")) {
+                                    if (!linkQueue.offer(poetryUrlHead + poetryLink)) {
+                                        System.out.println("---------------------------------LinkedBlockingQueue Offer False !!!");//push
+                                    } else {
+                                        System.out.println(poetryUrlHead + poetryLink);
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        int a = 0;
+    }
+}
