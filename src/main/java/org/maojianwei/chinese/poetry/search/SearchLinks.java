b877b6de81a343418557387b8aa9edf4e9f66568
maojianwei <maojianwei2016@126.com>
2016-04-09 11:18:25 +0800

    Complete: search links thread OK!

--- src/main/java/org/maojianwei/chinese/poetry/search/SearchLinks.java
+++ src/main/java/org/maojianwei/chinese/poetry/search/SearchLinks.java
@@ -7,73 +7,30 @@ import org.jsoup.select.Elements;
 
 import javax.swing.text.html.HTML;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Hello world!
  */
 public class SearchLinks {
-    public static final String poetryUrlHead = "http://so.gushiwen.org";
+
+    public static final String firstPagesuffix = "/type.aspx?p=1";
+    public static final String POETRY_URL_HEAD = "http://so.gushiwen.org";
+    public static final int MAX_PAGE_COUNT = 3;
+
 
     public static void main(String[] args) {
-        searchLinks();
-    }
-
-    public static void searchLinks() {
 
         LinkedBlockingQueue linkQueue = new LinkedBlockingQueue();
 
-        String pageLink = "http://so.gushiwen.org/type.aspx";
+        ExecutorService pool = Executors.newCachedThreadPool();
 
-        while (!pageLink.equals("")) {
-            try {
-                Document doc = Jsoup.connect(pageLink).get();
+        pool.submit(new SearchCallable(linkQueue, POETRY_URL_HEAD, firstPagesuffix, MAX_PAGE_COUNT));
 
-//                Elements elements = doc.getElementsByClass("sons");
-//
-//                for (Element div : elements) {
-//
-//                    for (Element ele : div.children()) {
-//                        if (!ele.tag().getName().equals("p")) {
-//                            continue;
-//                        } else {
-//                            if (ele.children() != null) {
-//                                String poetryLink = ele.child(0).attr("href");
-//                                if (!poetryLink.equals("")) {
-//                                    if (!linkQueue.offer(poetryUrlHead + poetryLink)) {
-//                                        System.out.println("---------------------------------LinkedBlockingQueue Offer False !!!");//push
-//                                    } else {
-//                                        System.out.println(poetryUrlHead + poetryLink);
-//                                    }
-//                                    break;
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-
-                Elements elements = doc.getElementsByClass("pages");
-
-                for(Element ele : elements.first().children()){
-                    if(!ele.tag().getName().equals("a")){
-                        continue;
-                    }else if(ele.text().equals("下一页")){
-
-                        if(!ele.attr("href").equals("")) {
-                            pageLink = poetryUrlHead + ele.attr("href");
-                            System.out.println(pageLink);
-                        }else{
-                            System.out.println("------------------Next Url False !!!");
-                        }
-                    }
-                    int aaa = 0;
-                }
-                int a = 0;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        int a = 0;
+        pool.shutdown();
     }
+
+
 }
